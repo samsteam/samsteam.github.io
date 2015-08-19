ba5e566ae2915d1261813eaf50a415d46721c43d
Bruno Cascio <brunocascio@gmail.com>
2015-08-19 20:49:11 -0300

    changes in about layout

--- js/filters.js
+++ js/filters.js
@@ -42,4 +42,16 @@ angular.module('sams.filters', [])
         result.push(i);
     return result;
   };
+})
+
+.filter('shuffle', function() {
+  return function(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+  }
 });
