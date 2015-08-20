f894e8690b1e42618af57a5ef2a98f8fc501ffd4
Cristian Sottile <cristian@MacBook-Pro-de-Cristian.local>
2015-08-19 21:21:33 -0300

    added angular-translate package

--- dist/js/sams.js
+++ dist/js/sams.js
@@ -425,14 +425,38 @@ app.config(function($translateProvider){
   | ---------------------------------------------------------------------------
   */
 
-  $translateProvider.useSanitizeValueStrategy('escaped');
+  $translateProvider.useSanitizeValueStrategy(null);
 
   $translateProvider.translations('en', {
     ABOUT_TITLE: 'Devteam',
+    ABOUT_ABOUTSAMS_TITLE: 'About SAMS <small style="color:#488FE7">and it\'s roots</small>',
+    ABOUT_TUTORIAL_TITLE: 'Tutorial<small style="color:#488FE7"></small>',
+    ABOUT_GITHUB_TITLE: 'Project\'s Github Information <small style="color:#488FE7">Fork it!</small>',
+    ABOUT_SAMS: 'About SAMS',
+    TUTORIAL: 'Tutorial',
+    GITHUB: 'GitHub',
+    HOME_START: 'START',
+    HOME_DOWNLOAD: 'DOWNLOAD',
+    HOME_ABOUT: 'ABOUT',
+    BREADCRUMB_REQUIREMENTS: 'Requirements',
+    BREADCRUM_POLICIES: 'Policies',
+    BREADCRUMB_RESOLUTION: 'Resolution',
   });
 
   $translateProvider.translations('es', {
-    ABOUT_TITLE: 'Devteam',
+    ABOUT_TITLE: 'Desarrolladores',
+    ABOUT_ABOUTSAMS_TITLE: 'Acerca de SAMS <small style="color:#488FE7">y sus comienzos</small>',
+    ABOUT_TUTORIAL_TITLE: 'Tutorial<small style="color:#488FE7"></small>',
+    ABOUT_GITHUB_TITLE: 'Información del Github del proyecto <small style="color:#488FE7">¡Forkealo!</small>',
+    ABOUT_SAMS: 'Acerca de SAMS',
+    TUTORIAL: 'Tutorial',
+    GITHUB: 'GitHub',
+    HOME_START: 'EMPEZAR',
+    HOME_DOWNLOAD: 'DESCARGAR',
+    HOME_ABOUT: 'ACERCA DE',
+    BREADCRUMB_REQUIREMENTS: 'Requerimientos',
+    BREADCRUM_POLICIES: 'Políticas',
+    BREADCRUMB_RESOLUTION: 'Resolución',
   });
 
   $translateProvider.preferredLanguage( 'es' );
